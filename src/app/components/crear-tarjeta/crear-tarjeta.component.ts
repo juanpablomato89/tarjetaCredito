48dadfec0329bd14b8dfd686d814ed059b8d841e
Juan Pablo Mato Hernandez <juanpablomato89@gmail.com>
2024-06-09 17:04:43 -0400

    asociando el formulario con el html y creando validaciones de los elmentos del form

--- src/app/components/crear-tarjeta/crear-tarjeta.component.ts
+++ src/app/components/crear-tarjeta/crear-tarjeta.component.ts
@@ -11,14 +11,16 @@ export class CrearTarjetaComponent implements OnInit {
   formCrearTarjeta: FormGroup;
   constructor(private fb: FormBuilder) {
     this.formCrearTarjeta = this.fb.group({
-      titular: ['', Validators.required],
-      numeroTarjeta: ['', Validators.required],
-      fechaExpiracion: ['', Validators.required],
+      titular: ['', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]],
+      numeroTarjeta: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
+      fechaExpiracion: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]],
       cvv: ['', Validators.required],
     })
   }
 
   ngOnInit(): void {
   }
+
+  crearTarjeta() { }
 
 }
