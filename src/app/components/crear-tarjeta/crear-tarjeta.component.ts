ddc6c4dbfae90309b6e2d38f6350bce50786327d
Juan Pablo Mato Hernandez <juanpablomato89@gmail.com>
2024-06-09 17:37:49 -0400

    validacion campos de los formularios utilizando html y css

--- src/app/components/crear-tarjeta/crear-tarjeta.component.ts
+++ src/app/components/crear-tarjeta/crear-tarjeta.component.ts
@@ -11,16 +11,19 @@ export class CrearTarjetaComponent implements OnInit {
   formCrearTarjeta: FormGroup;
   constructor(private fb: FormBuilder) {
     this.formCrearTarjeta = this.fb.group({
-      titular: ['', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]],
-      numeroTarjeta: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
-      fechaExpiracion: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]],
-      cvv: ['', Validators.required],
+      titular: ['', [Validators.required]],
+      numeroTarjeta: ['', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]],
+      fechaExpiracion: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
+      cvv: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]],
     })
   }
 
   ngOnInit(): void {
   }
 
-  crearTarjeta() { }
+  crearTarjeta() {
+    console.log(this.formCrearTarjeta);
+
+  }
 
 }
